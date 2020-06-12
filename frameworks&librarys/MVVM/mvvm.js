66e2d8c8cc58ac5ef59400d617c9d7ce5e9c4560
NARUTOne <wznaruto326@163.com>
2020-06-12 14:16:12 +0800

    mvvm

---
+++ frameworks&librarys/MVVM/mvvm.js
@@ -0,0 +1,16 @@
+/**
+ * @description mvvm body
+ */
+
+class MVVM {
+  constructor(options) {
+      this.$el = options.el;
+      this.$data = options.data;
+
+      //是否存在要编译的模板，存在则开始编译
+      if (this.$el) {
+          new Observer(this.$data);
+          new Compile(this.$el, this);
+      }
+  }
+}
