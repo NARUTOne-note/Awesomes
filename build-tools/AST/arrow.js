6ab658dbb0ecd1ba71c6de2047e80c08597c47a1
NARUTOne <wznaruto326@163.com>
2020-06-28 16:58:25 +0800

    ast

--- build-tools/AST/arrow.js
+++ build-tools/AST/arrow.js
@@ -1,7 +1,7 @@
 //babel 核心库，用来实现核心转换引擎
 const babel = require('@babel/core')
 //类型判断，生成AST零部件
-const types = require('babel-types')
+const types = require('@babel/types')
 
 //源代码
 const code = `const sum=(a,b)=>a+b;` //目标代码 const sum = function(a,b){ return a + b }
