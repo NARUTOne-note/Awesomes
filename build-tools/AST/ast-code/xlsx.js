e61f65c7dd3bb96fe7726848bd32df5c5654d870
NARUTOne <wznaruto326@163.com>
2023-12-11 13:35:37 +0800

    feat: 调试 xlsx

--- build-tools/AST/ast-code/xlsx.js
+++ build-tools/AST/ast-code/xlsx.js
@@ -1,4 +1,24 @@
-import xlsx from 'xlsx';
+const fs = require('fs')
+const { resolve } = require('path')
+const { Buffer } =  require('buffer')
+const nodeXlsx = require('node-xlsx').default
+const iconv = require('iconv-lite');
+iconv.skipDecodeWarning = true;
+
+const INPUT_CODE = resolve(process.cwd(), './demo/xlsx.json')
+const INPUT_XLSX = resolve(process.cwd(), './demo/test.xlsx')
+const OUTPUT_FOLDER = resolve(process.cwd(), './output')
+const CONSOLE_JSON = resolve(process.cwd(), './json')
+
+const jsonCode = fs.readFileSync(`${INPUT_CODE}`, 'utf-8')
+
+function createFile (filename, code) {
+  fs.writeFileSync(`${OUTPUT_FOLDER}/${filename}.xlsx`, code, 'utf-8')
+}
+
+function consoleJson (filename, res, type) {
+  fs.writeFileSync(`${CONSOLE_JSON}/${filename}.${type || 'txt'}`, JSON.stringify(res, null, 2), 'utf-8')
+}
 
 let arrayData = [
   ['name', 'age'],
@@ -6,65 +26,18 @@ let arrayData = [
   ['lisi', 21],
   ['wangwu', 22],
   ['zhaoliu', 23],
-  ['sunqi', 24],
+  ['sunqi', '山东高速搭嘎是否'],
 ];
 
-let jsonData = [{
-  name: "zhangsan1",
-  age: 30
-}, {
-  name: "lisi1",
-  age: 31
-}, {
-  name: "wangwu1",
-  age: 32
-}, {
-  name: "zhaoliu1",
-  age: 33
-}, {
-  name: "sunqi1",
-  age: 34,
-  length: "hello"
-}];
-
-// 将数据转成workSheet
-let jsonWorkSheet = xlsx.utils.json_to_sheet(jsonData);
-// console.log(arrayWorkSheet);
-// console.log(jsonWorkSheet);
-// 构造workBook
-let workBook = {
-  SheetNames: ['jsonWorkSheet'],
-  Sheets: {
-    'jsonWorkSheet': jsonWorkSheet,
+const buffer = nodeXlsx.build([
+  {
+    name: 'sheet1',
+    data: arrayData
   }
-};
-// 将workBook写入文件
-xlsx.writeFile(workBook, "./aa.xlsx");
+], {sheetOptions: {'!cols': [{wch: 20}, {wch: 30}]}});
+createFile('xlsx-export', buffer)
 
+const sheetData = nodeXlsx.parse(INPUT_XLSX)
 
-// 读取
-let workbook = xlsx.readFile('./aa.xlsx');
-let sheetNames = workbook.SheetNames;
-// 获取第一个workSheet
-let sheet1 = workbook.Sheets[sheetNames[0]];
-// console.log(sheet1);
-
-let range = xlsx.utils.decode_range(sheet1['!ref']);
-
-//循环获取单元格值
-for (let R = range.s.r; R <= range.e.r; ++R) {
-  let row_value = '';
-  for (let C = range.s.c; C <= range.e.c; ++C) {
-    let cell_address = {c: C, r: R}; //获取单元格地址
-    let cell = xlsx.utils.encode_cell(cell_address); //根据单元格地址获取单元格
-    //获取单元格值
-    if (sheet1[cell]) {
-      // 如果出现乱码可以使用iconv-lite进行转码
-      // row_value += iconv.decode(sheet1[cell].v, 'gbk') + ", ";
-      row_value += sheet1[cell].v + ", ";
-    } else {
-      row_value += ", ";
-    }
-  }
-  console.log(row_value);
-}
+consoleJson('xlsx-parse', JSON.parse(iconv.decode(JSON.stringify(sheetData), 'utf8')), 'json')
+consoleJson('xlsx-json', JSON.parse(jsonCode), 'json')
